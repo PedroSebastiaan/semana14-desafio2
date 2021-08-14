7b75b4d502620e8acacc3a2d0e9f67b749174963
PedroSebastiaan <pizarropedrov@gmail.com>
2021-08-14 19:01:26 -0400

    add: admin changes

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,11 +6,31 @@ class UsersController < ApplicationController
         @stories = Story.where(user_id: @user.id)
     end
 
+    def panel
+        @users = User.all
+    end
+
+    def modify
+        @user = User.find(params[:id])
+
+        if @user.admin? 
+            @user.update(admin: false)
+        else
+            @user.update(admin: true)
+        end
+
+        respond_to do |format|
+            format.html { redirect_to admin_panel_path, notice: 'Status changed' }
+        end
+    end
+
+    private
+
     def set_user
         @user = User.find(params[:id])
     end
 
     def user_params
-        params.require(:user).permit(:name, :email, :password, :password_confirmation)
+        params.require(:user).permit(:name, :email, :password, :password_confirmation, :admin)
     end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,13 @@
 Rails.application.routes.draw do
   devise_for :users, controllers: {
         sessions: 'users/sessions',
-        registrations: 'users/registrations'
+        registrations: 'users/registrations'  
       }
   resources :stories
   root 'stories#index'
   get 'users/:id', to: 'users#show', as: 'profile'
+  patch 'change/:id', to: 'users#modify', as: 'change'
+  get 'admin/panel', to: 'users#panel'
+  
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
