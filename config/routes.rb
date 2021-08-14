ad9204f2eb33b57c7d39bcb71039fcb4857237a2
PedroSebastiaan <pizarropedrov@gmail.com>
2021-08-14 13:18:33 -0400

    add: devise routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 Rails.application.routes.draw do
-  devise_for :users
+  devise_for :users, controllers: {
+        sessions: 'users/sessions'
+      }
   resources :stories
   root 'stories#index'
+  get 'users/sign_up', to: 'users#new'
+  get 'users/:id', to: 'users#show', as: 'profile'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
