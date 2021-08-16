aeff169e2d3bb14ee97e56a591288e4f36cd3f16
PedroSebastiaan <pizarropedrov@gmail.com>
2021-08-16 11:08:32 -0400

    second try with login

--- config/initializers/omniauth.rb
+++
@@ -1,5 +0,0 @@
-OmniAuth.config.logger = Rails.logger
-
-Rails.application.config.middleware.use OmniAuth::Builder do
-    provider :facebook, '200561268718503', '2641a290323a12fc52f814bb492092e5'
-end
